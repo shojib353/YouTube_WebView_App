9962d2ffd6293c9a74475e6b6e1c7504ee6313f7
HP <shojib.shojib10@gmail.com>
2024-07-05 12:36:59 +0600

    done

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.youtube
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +28,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var myWebView: WebView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var handler:Handler
+    private lateinit var mainContainer: FrameLayout
+    private lateinit var fullscreenContainer: FrameLayout
+
+    private var customView: View? = null
+    private var customViewCallback: WebChromeClient.CustomViewCallback? = null
+    private var isFullscreen: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +49,9 @@ class MainActivity : AppCompatActivity() {
 
         myWebView = findViewById(R.id.webview)
         swipeRefreshLayout = findViewById(R.id.swipeContainer)
+        mainContainer = findViewById(R.id.main_container)
+        fullscreenContainer = findViewById(R.id.fullscreen_container)
+
         val webSettings: WebSettings = myWebView.settings
         webSettings.javaScriptEnabled = true // Enable JavaScript if needed'
 
@@ -49,26 +64,112 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-        loadWebPage()
+
+
+
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                if (customView != null) {
+                    callback?.onCustomViewHidden()
+                    return
+                }
+
+                customView = view
+                customViewCallback = callback
+
+                fullscreenContainer.addView(customView)
+                fullscreenContainer.visibility = View.VISIBLE
+                mainContainer.visibility = View.GONE
+                swipeRefreshLayout.visibility = View.GONE
+
+
+                isFullscreen = true
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                hideSystemUI()
+
+                customView?.layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT
+                )
+
+            }
+
+            override fun onHideCustomView() {
+                if (customView == null) {
+                    return
+                }
+
+                fullscreenContainer.removeView(customView)
+                customView = null
+                fullscreenContainer.visibility = View.GONE
+                mainContainer.visibility = View.VISIBLE
+                swipeRefreshLayout.visibility = View.VISIBLE
+
+                customViewCallback?.onCustomViewHidden()
+
+                isFullscreen = false
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+                showSystemUI()
+            }
+        }
+
+
+
+        loadWebPage("https://www.youtube.com")
 
         swipeRefreshLayout.setOnRefreshListener {
-            loadWebPage()
+            refreshCurrentPage()
         }
 
 
 
     }
 
-    private fun loadWebPage(){
+    private fun refreshCurrentPage() {
+        val currentUrl = myWebView.url
         if (isNetworkAvailable()) {
-            myWebView.webViewClient = WebViewClient() // Ensures links open within the WebView
-            myWebView.loadUrl("https://www.youtube.com") // Replace with your URL
+            myWebView.reload()
+        } else {
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show()
+            myWebView.loadUrl("file:///android_asset/offline.html")
+            swipeRefreshLayout.isRefreshing = false
+        }
+    }
 
-            handler.postDelayed({
-                // Code to be executed after delay
-                // Replace this with your function call
+
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
+
+    private fun showSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_VISIBLE)
+        }
+    }
+
+
+
+    private fun loadWebPage(url: String) {
+        if (isNetworkAvailable()) {
+            myWebView.loadUrl(url)
+             handler.postDelayed({
+
                 swipeRefreshLayout.isRefreshing = false
-            }, 5000)
+            }, 3000)
 
         } else {
             Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show()
@@ -78,6 +179,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
     override fun onBackPressed() {
         if (myWebView.canGoBack()) {
             myWebView.goBack()
@@ -86,20 +189,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun isNetworkAvailable(): Boolean {
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network = connectivityManager.activeNetwork ?: return false
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                else -> false
+
+
+
+        private fun isNetworkAvailable(): Boolean {
+            val connectivityManager =
+                getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val network = connectivityManager.activeNetwork ?: return false
+                val activeNetwork =
+                    connectivityManager.getNetworkCapabilities(network) ?: return false
+                return when {
+                    activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            } else {
+                @Suppress("DEPRECATION")
+                val networkInfo = connectivityManager.activeNetworkInfo
+                return networkInfo != null && networkInfo.isConnected
             }
-        } else {
-            @Suppress("DEPRECATION")
-            val networkInfo = connectivityManager.activeNetworkInfo
-            return networkInfo != null && networkInfo.isConnected
         }
+
+            override fun onConfigurationChanged(newConfig: android.content.res.Configuration) {
+                super.onConfigurationChanged(newConfig)
+                if (isFullscreen && newConfig.orientation != android.content.res.Configuration.ORIENTATION_LANDSCAPE)
+                {
+                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                }
+
+            }
     }
-}
